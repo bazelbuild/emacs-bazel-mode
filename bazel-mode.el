ce093d926675377ff3ef3ced7664d87db14f4805
Robert Brown <robert.brown@gmail.com>
2018-04-09 19:09:18 -0400

    Add GPLv3 license file and copyright notice.

--- bazel-mode.el
+++ bazel-mode.el
@@ -1,5 +1,19 @@
+;;; bazel-mode.el       -*- lexical-binding:t -*-
 
-;;; bazel-mode.el
+;; Copyright (C) 2018 Robert E. Brown.
+
+;; Bazel Mode is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; Bazel Mode is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Bazel Mode.  If not, see <http://www.gnu.org/licenses/>.
 
 (require 'cl)
 (require 'python)
