2cf143b616df3de4b199538341d674c58386719e
Martin Blais <blais@furius.ca>
2020-06-27 12:25:20 -0400

    Recognize WORKSPACE.bazel files, not just WORKSPACE. (#80)

--- lisp/bazel-util.el
+++ lisp/bazel-util.el
@@ -27,7 +27,8 @@
 If FILE-NAME is not in a Bazel workspace, return nil.  Otherwise,
 the return value is a directory name."
   (cl-check-type file-name string)
-  (let ((result (locate-dominating-file file-name "WORKSPACE")))
+  (let ((result (or (locate-dominating-file file-name "WORKSPACE")
+                    (locate-dominating-file file-name "WORKSPACE.bazel"))))
     (and result (file-name-as-directory result))))
 
 (defun bazel-util-package-name (file-name workspace-root)
