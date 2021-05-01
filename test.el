15c4c12038bf493ccb66fb4cdded962a0bbf0211
Philipp Stephani <phst@google.com>
2021-05-02 00:36:38 +0200

    Fix Edebug specification of ‘bazel-test--with-file-buffer’ macro.
    We evaluate all arguments, so the specification for the first argument
    shouldn’t be ‘sexp’.

--- test.el
+++ test.el
@@ -162,7 +162,7 @@ BODY finishes."
   "Visit FILENAME in a temporary buffer.
 Execute BODY with the buffer that visits FILENAME current.  Kill
 that buffer once BODY finishes."
-  (declare (indent 1) (debug (sexp body)))
+  (declare (indent 1) (debug t))
   (let ((buffer (make-symbol "buffer")))
     `(let ((,buffer (find-file-noselect ,filename)))
        (unwind-protect
