5ec0ac36fa0c4a3922d1dd0839de74cee8c6066a
Philipp Stephani <phst@google.com>
2021-05-01 23:07:38 +0200

    Remove INITIAL-INPUT argument.
    As https://www.gnu.org/software/emacs/manual/html_node/elisp/Initial-Input.html
    explains, this is discouraged.

--- bazel.el
+++ bazel.el
@@ -1202,12 +1202,11 @@ COMMAND is a Bazel command to be included in the minibuffer prompt."
          (package-name
           (or (bazel--package-name file-name workspace-root)
               (user-error "Not in a Bazel package.  No BUILD file found")))
-         (initial-input (concat "//" package-name))
          (prompt (combine-and-quote-strings
                   (append bazel-command (list command ""))))
          (table
           (bazel--target-pattern-completion-table workspace-root package-name)))
-    (completing-read prompt table nil nil initial-input)))
+    (completing-read prompt table)))
 
 ;;;; Utility functions to work with Bazel workspaces
 
