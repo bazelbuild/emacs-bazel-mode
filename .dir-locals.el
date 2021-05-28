8ac92f5f22a018f5197b7eeeebb3f47efacbea92
Philipp Stephani <phst@google.com>
2021-05-28 14:16:30 +0200

    Use 80-column lines for all files.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((emacs-lisp-mode . ((mode . bug-reference-prog)
-                     (bug-reference-url-format . "https://debbugs.gnu.org/cgi/bugreport.cgi?bug=%s"))))
+                     (bug-reference-url-format . "https://debbugs.gnu.org/cgi/bugreport.cgi?bug=%s")))
+ (nil . ((fill-column . 80))))
