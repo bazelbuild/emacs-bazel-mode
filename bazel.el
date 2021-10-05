cb160e8161b561f995c6e3e71fd8d190f12859a2
Yesudeep Mangalapilly <yesudeep@gmail.com>
2021-10-04 21:08:32 -0700

    Use bazel mode for .bazel files.

--- bazel.el
+++ bazel.el
@@ -358,7 +358,7 @@ This is the parent mode for the more specific modes
 ;;;###autoload
 (add-to-list 'auto-mode-alist
              ;; https://docs.bazel.build/versions/3.0.0/build-ref.html#packages
-             (cons (rx ?/ (or "BUILD" "BUILD.bazel") eos) #'bazel-build-mode))
+             (cons (rx (or "/BUILD" ".bazel") eos) #'bazel-build-mode))
 
 ;;;###autoload
 (define-derived-mode bazel-workspace-mode bazel-mode "Bazel WORKSPACE"
